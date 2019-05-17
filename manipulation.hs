7a418562843ad586bc5d78fd2bbe04650897d7c1
unknown <trevllew@gmail.com>
2019-05-16 21:22:41 -0400

    gittask3

--- manipulation.hs
+++ manipulation.hs
@@ -4,3 +4,7 @@ reverseSecond :: [String] -> [String]
 reverseSecond [] = []
 reverseSecond [x] = [x]
 reverseSecond (x:y:xs) = x : (reverse y) : reverseSecond xs
+
+trial xs 
+  | (length xs >= 20) = take 20 xs : ( trial (drop 20 xs) )
+  | otherwise = take (length xs) xs : []
